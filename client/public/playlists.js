2b21c65cccd848a7777930e5af477af1180eabcf
wendy-guo <wendy.guo@live.com>
2020-09-16 11:01:21 -0400

    added react component template

---
+++ client/public/playlists.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const e = React.createElement;
+
+class Playlists extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return "You liked this.";
+    }
+
+    return <button onClick={() => this.setState({ liked: true })}>Like</button>;
+  }
+}
