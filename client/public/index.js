4492156c05e2c8d46030bcf9c1a5bed7ae0e46fb
wendy-guo <wendy.guo@live.com>
2020-09-24 13:52:07 -0400

    waited for api requests and sorted data

--- client/public/index.js
+++ client/public/index.js
@@ -1,9 +1,4 @@
 (function () {
-  /**
-   * Obtains parameters from the hash of the URL
-   * @return Object
-   */
-
   console.log("hello ^^");
   function getHashParams() {
     var hashParams = {};
@@ -60,14 +55,62 @@
           let playlistAlbumData = new Map();
           let playlistAlbumYearData = new Map();
 
-          const analyzePlaylist = () => {
-            let keywords = new Map();
-            playlistArtistData = new Map(
-              [...playlistArtistData.entries()].sort((a, b) => a[1] - b[1])
-            );
+          let sortedArtistData = new Map();
+          let sortedArtistGenreData = new Map();
+          let sortedAlbumData = new Map();
+          let sortedAlbumYearData = new Map();
 
-            $("#loading").hide();
-            $("#analysis").show();
+
+          const analyzePlaylist = () => {
+            console.log(playlistArtistData);
+
+            // sort all the maps in asc ending order for frequency
+            new Promise((resolve) => {
+              sortedArtistData = Object.entries(playlistArtistData).sort(
+                ([, a], [, b]) => b - a
+              );
+              sortedArtistGenreData = Object.entries(
+                playlistArtistGenreData
+              ).sort(([, a], [, b]) => b - a);
+              sortedAlbumData = Object.entries(playlistAlbumData).sort(
+                ([, a], [, b]) => b - a
+              );
+              sortedAlbumYearData = Object.entries(playlistAlbumYearData).sort(
+                ([, a], [, b]) => b - a
+              );
+
+              /** 
+              while (
+                Object.keys(sortedArtistData).length !=
+                  Object.keys(playlistArtistData).length ||
+                Object.keys(sortedArtistGenreData).length !=
+                  Object.keys(playlistArtistGenreData).length ||
+                Object.keys(sortedAlbumData).length !=
+                  Object.keys(playlistAlbumData).length ||
+                Object.keys(sortedAlbumYearData).length !=
+                  Object.keys(playlistAlbumYearData).length
+              ) {
+                console.log("sorting");
+              } */
+
+              while (
+                sortedArtistData.length !=
+                  Object.keys(playlistArtistData).length ||
+                sortedArtistGenreData.length !=
+                  Object.keys(playlistArtistGenreData).length ||
+                sortedAlbumData.length !=
+                  Object.keys(playlistAlbumData).length ||
+                sortedAlbumYearData.length !=
+                  Object.keys(playlistAlbumYearData).length
+              ) {
+                console.log("sorting");
+              }
+
+              console.log(Object.keys(playlistArtistData).length);
+              resolve();
+            }).then(() => {
+              //displayResults();
+            });
           };
 
           const handlePlaylistSubmit = (playlistNum) => {
@@ -83,48 +126,73 @@
                 var tracks = tracks_res.items;
                 var artistIdData = new Map();
                 //console.log("tracks", tracks);
-                tracks.forEach((tr) => {
-                  // get album to get genre
-                  tr.track.artists.forEach((artist) => {
-                    console.log(artist);
-                    artistIdData[artist.id] =
-                      (artistIdData[artist.id] || 0) + 1;
-                  });
-                  console.log("hi", tr.track.album);
-                  // album name data
-                  playlistAlbumData[tr.track.album.name] =
-                    (playlistAlbumData[tr.track.album.name] || 0) + 1;
-                  // album release year data
-                  playlistAlbumYearData[
-                    tr.track.album.release_date.slice(0, 4)
-                  ] =
-                    (playlistAlbumYearData[
+
+                new Promise((resolve) => {
+                  tracks.forEach((tr, i, arr) => {
+                    // get album to get genre
+                    tr.track.artists.forEach((artist) => {
+                      artistIdData[artist.id] =
+                        (artistIdData[artist.id] || 0) + 1;
+                    });
+                    // album name data
+                    playlistAlbumData[tr.track.album.name] =
+                      (playlistAlbumData[tr.track.album.name] || 0) + 1;
+                    // album release year data
+                    playlistAlbumYearData[
                       tr.track.album.release_date.slice(0, 4)
-                    ] || 0) + 1;
-                });
+                    ] =
+                      (playlistAlbumYearData[
+                        tr.track.album.release_date.slice(0, 4)
+                      ] || 0) + 1;
 
-                Object.keys(artistIdData).forEach((artistId) => {
-                  $.ajax({
-                    url: `https://api.spotify.com/v1/artists/${artistId}`,
-                    headers: {
-                      Authorization: "Bearer " + access_token,
-                    },
-                    success: function (artist_res) {
-                      playlistArtistData[artist_res.name] =
-                        artistIdData[artistId];
-                      artist_res.genres.forEach((genre) => {
-                        playlistArtistGenreData[genre] =
-                          (playlistArtistGenreData[genre] || 0) + 1;
-                      });
-                    },
+                    if (i === arr.length - 1) {
+                      resolve();
+                    }
                   });
-                });
+                })
+                  .then(() => {
+                    return new Promise((resolve) => {
+                      Object.keys(artistIdData).forEach((artistId, i, arr) => {
+                        $.ajax({
+                          url: `https://api.spotify.com/v1/artists/${artistId}`,
+                          headers: {
+                            Authorization: "Bearer " + access_token,
+                          },
+                          success: function (artist_res) {
+                            playlistArtistData[artist_res.name] =
+                              artistIdData[artistId];
+                            artist_res.genres.forEach((genre) => {
+                              playlistArtistGenreData[genre] =
+                                (playlistArtistGenreData[genre] || 0) + 1;
+                            });
 
-                console.log(playlistArtistData);
-                console.log(playlistArtistGenreData);
-                console.log(playlistAlbumData);
-                console.log(playlistAlbumYearData);
-                analyzePlaylist();
+                            console.log(playlistArtistData);
+
+                            if (i === arr.length - 1) {
+                              while (
+                                Object.keys(playlistArtistData).length !=
+                                i + 1
+                              ) {
+                                console.log(
+                                  "haiiii",
+                                  Object.keys(playlistArtistData).length
+                                );
+                              }
+                              console.log(i, Object.keys(playlistArtistData));
+                              resolve();
+                            }
+                          },
+                        });
+                      });
+                    });
+                  })
+                  .then(() => {
+                    console.log(playlistArtistData);
+                    console.log(playlistArtistGenreData);
+                    console.log(playlistAlbumData);
+                    console.log(playlistAlbumYearData);
+                    analyzePlaylist();
+                  });
               },
             });
           };
